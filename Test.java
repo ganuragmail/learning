a49c0d900f4c26a3dacb131424d86bd2790d1799
ganuragmail <158707046+ganuragmail@users.noreply.github.com>
2024-11-10 03:27:48 +0000

    changed test

--- Test.java
+++ Test.java
@@ -1,5 +1,9 @@
 class Test{
   public static void main(String x[]){
     System.out.println("Hello world!!!");
+    int arr[] = {1,2,3,4,5,6};
+    for(int a :  arr){
+      System.out.println(a);
+    }
   }
 }
